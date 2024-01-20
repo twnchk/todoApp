fbfea973be6957aae7746675caa145adb1d4e284
Tomasz Wańczyk <facebaster@gmail.com>
2024-01-20 17:26:20 +0100

    Display checkboxes for allowed groups when creating new board

--- todoBoard/forms.py
+++ todoBoard/forms.py
@@ -6,8 +6,6 @@ class CreateTaskForm(forms.ModelForm):
     class Meta:
         model = TodoItem
         fields = '__all__'
-        ''' TODO: remove 'author' field after users functionality is added,
-                  change to predefined value for currently logged user '''
 
     def __init__(self, *args, init_board_id, user_id, **kwargs):
         super(CreateTaskForm, self).__init__(*args, **kwargs)
@@ -22,3 +20,6 @@ class CreateBoardForm(forms.ModelForm):
     class Meta:
         model = TodoList
         fields = '__all__'
+        widgets = {
+            'allowed_groups': forms.CheckboxSelectMultiple
+        }
