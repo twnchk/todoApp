8d9cbf5fba614cd17ff862846316498aeeb206ec
twnchk <facebaster@gmail.com>
2024-04-18 20:11:22 +0200

    Implement password change & reset functionality, minor bugfixes

--- todoPlanner/settings.py
+++ todoPlanner/settings.py
@@ -146,3 +146,10 @@ AUTH_USER_MODEL = 'users.CustomUser'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,27 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 from .views import register, user_login, user_logout, user_profile
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', user_login, name='user_login'),
     path('logout/', user_logout, name='user_logout'),
+    path('changePassword/', auth_views.PasswordChangeView.as_view(template_name="change_password.html"),
+         name='password_change'),
+    path('changePassword/done', auth_views.PasswordChangeDoneView.as_view(template_name="change_password_done.html"),
+         name='password_change_done'),
+    path('resetPassword/', auth_views.PasswordResetView.as_view(template_name="reset_password.html"),
+         name='password_reset'),
+    path('resetPassword/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='confirm_reset_password.html'),
+         name='password_reset_confirm'),
+    path('resetPassword/done/', auth_views.PasswordResetDoneView.as_view(template_name="reset_password_done.html"),
+         name='password_reset_done'),
+    path('resetPassword/complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name="reset_password_complete.html"),
+         name='password_reset_complete'),
     path('profile/<int:id>', user_profile, name='user_profile'),
 ]
 
