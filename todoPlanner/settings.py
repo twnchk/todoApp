0cc91170fa0b30e7c569fc119058b66e42438f2a
twnchk <facebaster@gmail.com>
2024-04-20 17:38:59 +0200

    Minor fix

--- todoPlanner/settings.py
+++ todoPlanner/settings.py
@@ -153,7 +153,7 @@ LOGIN_URL = '/login/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = env.str('EMAIL_HOST')
-EMAIL_PORT = env.int('EMAIL_PORT')
+EMAIL_PORT = env.str('EMAIL_PORT')
 EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
