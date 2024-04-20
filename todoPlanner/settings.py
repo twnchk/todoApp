5c38ac0036428c87416140fad180b8b49f09e887
twnchk <facebaster@gmail.com>
2024-04-20 17:44:32 +0200

    update ALLOWED_HOSTS

--- todoPlanner/settings.py
+++ todoPlanner/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".railway.app.com", "localhost", "127.0.0.1", "0.0.0.0"]
+ALLOWED_HOSTS = [".railway.app", "localhost", "127.0.0.1", "0.0.0.0"]
 
 
 # Application definition
